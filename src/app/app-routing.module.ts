3e0d4d14a14d15693803b41585e1f00afcf74ce5
Kshitij Kumar <imkshitij@Kshitijs-MacBook-Air-2.local>
2025-02-05 22:53:12 +0530

    video12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { SellerComponent } from './seller/seller.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
 import { sellerAuthGuard } from './seller-auth.guard';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
 
 const routes: Routes = [
   { component: HomeComponent, path: ''},
   { component: SellerComponent, path: 'seller-auth'},
-  { component: SellerHomeComponent, path: 'seller-home', canActivate : [sellerAuthGuard]}
+  { component: SellerHomeComponent, path: 'seller-home', canActivate : [sellerAuthGuard]},
+  { component: SellerAddProductComponent, path: 'seller-add-product', canActivate : [sellerAuthGuard]}
 ];
 
 @NgModule({
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,15 +8,19 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+
 export class HeaderComponent implements OnInit{
-  menuType : String = 'default';
+  menuType : string = 'default';
+  sellerName : string = '';
   constructor(private route : Router){}
 
   ngOnInit(): void {
     this.route.events.subscribe((val : any) => {
         if(val.url){
           if(localStorage.getItem('seller') && val.url.includes('seller')){
-            console.warn("in seller area");
+            let sellerLocalStorage = localStorage.getItem('seller');
+            let sellerData = sellerLocalStorage && JSON.parse(sellerLocalStorage)[0];
+            this.sellerName = sellerData.name;
             this.menuType = "seller"
           }
           else{
@@ -26,5 +30,8 @@ export class HeaderComponent implements OnInit{
         }
     })
   }
-
+  logout():void{
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
+  }
 }
